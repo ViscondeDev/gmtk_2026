f8fb64f3521ac66c29fc0abc3c314c597d7bd922
Chigozie Oduah <oduahchigozie46@gmail.com>
2026-07-23 19:01:07 +0100

    fixed level transition and added game base component

--- scenes/controller.gd
+++ scenes/controller.gd
@@ -1,15 +1,13 @@
 extends Node
 
 const loading_scene = preload("res://scenes/ui scenes/Loading.tscn")
-var current_scene = load("res://scenes/ui scenes/main_screen/MainScreen.tscn")
+var current_scene : PackedScene = null
 var current_instance: Node = null
 
 @onready var loadscreen_instance = loading_scene.instantiate()
 
 func _ready() -> void:
-	current_instance = current_scene.instantiate()
-	add_child(current_instance)
-	current_instance.main_screen_action.connect(_main_scene_action)
+	_transition_scene(SceneType.MAIN)
 	
 	
 enum MainSceneAction {
@@ -18,52 +16,52 @@ enum MainSceneAction {
 	CREDITS = 2
 }
 func _main_scene_action(type: MainSceneAction):
-	remove_child(current_instance)
-	add_child(loadscreen_instance)
-	
 	match type:
 		MainSceneAction.START:
-			_load_game(1)
+			print("Game not created yet")
 		MainSceneAction.LEVEL_SELECT:
-			_load_level_select()
+			_transition_scene(SceneType.LEVEL_SELECT)
 		MainSceneAction.CREDITS:
-			_load_credits()
+			print("Credits not created yet")
 
-
-func _load_game(_level: int):
-	print("Game not created yet")
-	remove_child(loadscreen_instance)
-	add_child(current_instance)
-
-
-func _load_level_select():
-	current_scene = load("res://scenes/ui scenes/level_select/level_select.tscn")
-	current_instance = current_scene.instantiate()
-	current_instance.level_select_action.connect(_level_select_action)
-	remove_child(loadscreen_instance)
-	add_child(current_instance)
-
-
-func _load_credits():
-	print("Credits not created yet")
-	remove_child(loadscreen_instance)
-	add_child(current_instance)
 
 enum LevelActionAction {
 	BACK = 0,
+	LEVEL_SELECT = 1,
 }
-func _level_select_action(type: LevelActionAction):
+func _level_select_action(type: LevelActionAction, extra):
+	match type:
+		LevelActionAction.BACK:
+			_transition_scene(SceneType.MAIN)
+		LevelActionAction.LEVEL_SELECT:
+			print("Levels not added yet")
+			#_load_level(extra)
+	
+	
+enum SceneType {
+	MAIN,
+	LEVEL_SELECT,
+	GAME,
+}
+func _transition_scene(to: SceneType, level: int = 0):
 	remove_child(current_instance)
 	add_child(loadscreen_instance)
 	
-	match type:
-		LevelActionAction.BACK:
-			_back_to_main()
-	
-	
-func _back_to_main():
-	current_scene = load("res://scenes/ui scenes/main_screen/MainScreen.tscn")
-	current_instance = current_scene.instantiate()
-	current_instance.main_screen_action.connect(_main_scene_action)
+	match to:
+		SceneType.MAIN:
+			current_scene = load("res://scenes/ui scenes/main_screen/MainScreen.tscn")
+			current_instance = current_scene.instantiate()
+			current_instance.main_screen_action.connect(_main_scene_action)
+			
+		SceneType.LEVEL_SELECT:
+			current_scene = load("res://scenes/ui scenes/level_select/level_select.tscn")
+			current_instance = current_scene.instantiate()
+			current_instance.level_select_action.connect(_level_select_action)
+		
+		SceneType.GAME:
+			current_scene = load("res://scenes/ui scenes/board/board.tscn")
+			current_instance = current_scene.instantiate()
+			current_instance.level = level
+
 	remove_child(loadscreen_instance)
 	add_child(current_instance)
