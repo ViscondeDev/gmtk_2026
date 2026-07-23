ddd6eba7a6fa81f79f717dccb1e0ea96b14d91e3
ViscondeDev <willianoacpf@gmail.com>
2026-07-23 14:54:26 -0300

    attempt_defend

--- scenes/piece/piece.gd
+++ scenes/piece/piece.gd
@@ -5,7 +5,6 @@ extends Node2D
 @export var movement_type: Movement
 @export var is_friendly: bool = false
 
-var is_thretened: bool
 var current_board_position: Vector2i
 var possible_moves: Array[Vector2i]
 
