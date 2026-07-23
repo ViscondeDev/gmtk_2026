e438e43ed5e4c79cc6320b5ec5cbc86ad5f88a46
ViscondeDev <willianoacpf@gmail.com>
2026-07-23 11:57:58 -0300

    line break for readability

--- resources/movements/movement.gd
+++ resources/movements/movement.gd
@@ -3,7 +3,11 @@
 extends Resource
 
 
-static func _validate_cells_in_line(start_position: Vector2i, axis: Vector2i, is_friendly: bool) -> Array[Vector2i]:
+static func _validate_cells_in_line(
+	start_position: Vector2i,
+	axis: Vector2i,
+	is_friendly: bool,
+) -> Array[Vector2i]:
 	var valid_moves: Array[Vector2i]
 	var next_tile := start_position + axis
 	if next_tile in Board.current_board.tiles:
